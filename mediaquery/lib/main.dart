89b914bac9d4bb28c7a9e78fa08fb1eda6091365
river11456 <river11456@gmail.com>
2024-08-12 21:08:02 +0900

    initial commit

---
+++ mediaquery/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_design/mypage.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        textTheme: const TextTheme(
+          bodyMedium: TextStyle(
+            fontSize: 30,
+            color: Colors.white,
+          ),
+        ),
+      ),
+      home: const MyPage(),
+    );
+  }
+}
